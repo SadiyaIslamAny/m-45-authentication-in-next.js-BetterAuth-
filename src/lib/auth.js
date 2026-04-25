0771cf1929f02ccd0089833ed9c9e59d388a3b96
SADIA <you@example.com>
2026-04-25 11:51:43 +0600

    cmmit

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -3,9 +3,13 @@ import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
 const client = new MongoClient(process.env.AUTH_DB_URI);
+await client.connect();
 const db = client.db();
 
 export const auth = betterAuth({
+  database: mongodbAdapter(db, {
+    client,
+  }),
   emailAndPassword: {
     enabled: true,
   },
