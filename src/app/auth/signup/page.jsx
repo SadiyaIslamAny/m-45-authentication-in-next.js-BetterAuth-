7f2eab98e7e4a063f637188ce813fb7974c55a35
SADIA <you@example.com>
2026-04-25 10:31:58 +0600

    45-4 Setup auth route, HeroUI and Gravity UI icons

---
+++ src/app/auth/signup/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SignUpPage = () => {
+    return (
+        <div>
+            <h2>please sign up</h2>
+        </div>
+    );
+};
+
+export default SignUpPage;
--- src/lib/auth.js
+++ src/lib/auth.js
@@ -6,7 +6,10 @@ const client = new MongoClient(process.env.AUTH_DB_URI);
 const db = client.db();
 
 export const auth = betterAuth({
-   database: mongodbAdapter(db, {
+  emailAndPassword: {
+    enabled: true,
+  },
+  database: mongodbAdapter(db, {
     // Optional: if you don't provide a client, database transactions won't be enabled.
     client
   }),
